81b9ea82386f24f28b5c18acd34d4eedd1433cc3
carmen-raterman <ratersop@gmail.com>
2024-03-16 14:48:26 -0400

    added slugify and marked, added before attributes to article.js

--- models/article.js
+++ models/article.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose')
+//adding slugify to convert our title into a url friendly slug to use
+const slugify = require('slugify')
+//adding marked to create markdown and turn it into html
+const marked = require('marked')
 
 const articleSchema = new mongoose.Schema({
     title: {
@@ -15,7 +19,21 @@ const articleSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now /* gives current date as 'Created At' */
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true  
     }
 })
 
+//some before attributes
+articleSchema.pre('validate', function() {
+    if (this.title) {
+        this.slug = slugify(this.title, { lower: true, strict: true }) //this step makes the title lowercase and also removes any special characters (like a colon) that might be in the title
+        //we set this up to create a slug from our title for every time we validate
+    }
+
+    next()
+})
 module.exports = mongoose.model('Article', articleSchema)
