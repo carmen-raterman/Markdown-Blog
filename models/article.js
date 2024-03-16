e1fec1917402e4a0b1472535ae5f8566b6d34a29
carmen-raterman <ratersop@gmail.com>
2024-03-16 16:32:03 -0400

    updated markdown syntax in new post

--- models/article.js
+++ models/article.js
@@ -3,6 +3,12 @@ const mongoose = require('mongoose')
 const slugify = require('slugify')
 //adding marked to create markdown and turn it into html
 const marked = require('marked')
+//adding purifier which returns a function
+const createDomPurify = require('dompurify')
+//we put this in brackets because we only want the JSDOM portion of what is returns
+const { JSDOM } = require('jsdom')
+//allows our dompurifier to create HTML and purify it by using the JSDOM window object, sanitizes our html
+const dompurify = createDomPurify(new JSDOM().window)
 
 const articleSchema = new mongoose.Schema({
     title: {
@@ -24,6 +30,10 @@ const articleSchema = new mongoose.Schema({
       type: String,
       required: true,
       unique: true  
+    },
+    sanitizedHtml: {
+        type: String,
+        required: true
     }
 })
 
@@ -32,6 +42,10 @@ articleSchema.pre('validate', function(next) {
     if (this.title) {
         this.slug = slugify(this.title, { lower: true, strict: true }) //this step makes the title lowercase and also removes any special characters (like a colon) that might be in the title
         //we set this up to create a slug from our title for every time we validate
+    }
+
+    if (this.markdown) {
+        this.sanitizedHtml = dompurify.sanitize(marked(this.markdown))
     }
 
     next();
